55607c42cfafc65578853591fbc0ff4796a4d68e
Govinda Jay <govindalaw@gmail.com>
2023-12-11 20:46:25 -0800

    new files as of 12-11

--- reservation/views.py
+++ reservation/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.http import HttpResponse
+
+# def reservation(request):
+#     return HttpResponse("Thank you for your reservation")
+
+
+def index (request):
+    return render (request, 'index.html', {})
