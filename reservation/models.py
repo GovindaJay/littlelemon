34522cd606068584a67d976b49f9fec4e89b2992
Govinda Jay <govindalaw@gmail.com>
2023-12-12 22:01:33 -0800

    This is the second o

--- reservation/models.py
+++ reservation/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+
+class Booking (models.Model):
+    #id = models.IntegerField()
+    name = models.CharField(max_length=255)
+    No_of_guests = models.IntegerField()
+    BookingDate = models.DateTimeField()
+    
+    def __str__(self):
+      return f"{self.name}, {self.No_of_guests}, {self.BookingDate}"
+
+    
+
+class Menu(models.Model):
+
+    title= models.CharField(max_length=255, blank ="True")
+    price = models.DecimalField(max_digits=10, decimal_places=2, default = 0)
+    inventory = models.IntegerField(default=0)
+    
+    def __str__(self):
+      return f"{self.Title}, {self.price}"
+    
+    
+    
+    
+    
